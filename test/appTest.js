1ba80f830680516eb2317bd7a2bbb2cfe0f81a28
Bilal <glowinginthedark@github.com>
2019-05-03 05:55:58 -0700

    Add 2 new tests - for high scores and registering accounts

--- test/appTest.js
+++ test/appTest.js
@@ -19,3 +19,18 @@ describe('GET /', () => {
             });
       });
  });
+
+chai.should();
+
+describe("Test Registering an account", () => {
+	it("Testing with an existing email address... Registration should fail.", async () => {
+		assert((await backend.addAccount("crap_crap@crap.com", "UnHy`utkV{63;5[", "harry", "kane")).failed, 
+			"auth/email-already-in-use: The email address is already in use by another account.");
+	});
+});
+
+describe("Test retrieving high scores from firebase", () => {
+	it("Retrieving high scores... Result should be an array.", async () => {
+		assert(Array.isArray(await backend.getHighScores("big_or_small")), true);
+	});
+});
