671858d172bd508fdd351919c50b032447d74313
bhhe <bowen.he.work@gmail.com>
2019-05-03 11:31:18 -0700

    Endpoint test(In progress)
    Header/Head Partial not found during test.
    Footer Partial is found though

--- test/appTest.js
+++ test/appTest.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = require('chai').assert;
@@ -6,16 +7,25 @@ const should = require('chai').should;
 const request = require('supertest');
 chai.use(chaiHttp);
 const app = require('../app.js');
-const backend = require('../backend.js');
 
-describe('GET /', () => {
-  it('it should load page', (done) => {
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
+    }
+}
+
+describe('GET /', function(){
+    this.timeout(10000);
+    it('it should load page', function(done){
+        wait(5000);
         chai.request(app)
             .get('/')
-            .end(async (err, res) => {
-            	const result = await res.statusCode;
-            	const body = await res.body;
-            	done();
+            .end(function(err, res) {
+                console.log(err);
+                expect(res).to.have.status(200);
+                done();
             });
       });
  });
